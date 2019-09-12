8bff42bd6933251ed6db671c465ce7d7126542ce
root <munchell@gmail.com>
2019-09-12 09:39:07 +0000

    First draft

---
+++ README.md
@@ -0,0 +1,32 @@
+# Data Warehouse project
+
+## Table of Contents
+
+1. [Discuss the purpose of this database in the context of the startup, Sparkify, and their analytical goals](#discuss)
+2. [Redshift as both staging and dimension table database](#redshift)
+3. [How to run the scripts](#run)
+4. [Notes](#notes)
+
+## <a name="discuss"></a>Discuss the purpose of this database
+The purpose of this database is to provide Sparkify the necessary tool to retrieve vital, business oriented, information about their service. Their current main focus is to be able to, easily and fast, to retrieve information about which songs users are listening to. They do have information about this stored in logfiles already but it is not easy to search or aggregate data from these logfiles. In this assignment we have been asked to transform data from the logfiles together with meta data about songs (also stored in files) into a database design that will allow Sparkify to meet their business need related to finding out which songs their users are listening to.
+
+## <a name="run"></a>How to run the scripts
+In order for the scripts to run successfully it is required that an apprpriate IAM user is created in order to be able to access and read from the S3 buckets containing log data and song data.
+
+It is also necessary to setup the Redshift cluster used by the etl.py script in order for the script to run without errors.
+
+### Create an IAM user using your AWS Console
+Use your AWS Console to add a user that at least has S3 read rights.
+
+### Setup a Redshift Cluster using your AWS Console
+Use your AWS Console to setup a Redshift cluster.
+
+### Add the IAM and Redshift setup to the config file
+Copy the IAM ARN to the _dwh.cfg_ config file
+Copy the Redshift cluster configuration (HOST, DB_NAME, DB_USER, DB_PASSWORD and DB_PORT) to the _dwh.cfg_ config file
+
+### Run etl.py
+_python3 etl.py_
+
+## <a name="redshift"></a>Redshift as both staging and dimension table database
+In this project we will use Redshift to hold both the staging tables and the dimension and fact tables. The staging tables are just mirroring the logfile and songfile content into two tables; *log_events_table* and *song_table*. From the staging tables we will use COPY statements to transfer the content from the staging tables to the selected dimension and fact tables.
--- sql_queries.py
+++ sql_queries.py
@@ -7,60 +7,178 @@ config.read('dwh.cfg')
 
 # DROP TABLES
 
-staging_events_table_drop = ""
-staging_songs_table_drop = ""
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
+# First, define the staging tables. These tables will just mirror the
+# input data from the events and song data csv files
+
 staging_events_table_create= ("""
+CREATE TABLE IF NOT EXISTS staging_events (
+  event_id BIGINT IDENTITY(0,1) NOT NULL,
+  artist VARCHAR NOT NULL,
+  auth VARCHAR NOT NULL,
+  firstName VARCHAR NOT NULL,
+  gender VARCHAR NOT NULL,
+  itemInSession BIGINT NOT NULL,
+  lastName VARCHAR NOT NULL,
+  length DECIMAL(10,5) NOT NULL,
+  level VARCHAR NOT NULL,
+  location VARCHAR,
+  method VARCHAR,
+  page VARCHAR,
+  registration VARCHAR,
+  sessionId INTEGER NOT NULL SORTKEY DISTKEY,
+  song VARCHAR NOT NULL,
+  status INTEGER,
+  ts BIGINT NOT NULL,
+  userAgent VARCHAR,
+  userId INTEGER NOT NULL
+);
 """)
 
 staging_songs_table_create = ("""
+CREATE TABLE IF NOT EXISTS staging_songs (
+  num_songs INTEGER NOT NULL,
+  artist_id VARCHAR NOT NULL SORTKEY DISTKEY,
+  artist_latitude DECIMAL(9,5),
+  artist_longitude DECIMAL(9,5),
+  artist_location VARCHAR,
+  artist_name VARCHAR NOT NULL,
+  song_id VARCHAR NOT NULL,
+  title VARCHAR NOT NULL,
+  duration DECIMAL(10,5),
+  year INTEGER NOT NULL
+);
 """)
+
+# Create the dimension tables
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplay_table(
+   songplay_id INTEGER IDENTITY(0,1) NOT NULL SORTKEY,
+   start_time TIMESTAMP NOT NULL,
+   user_id VARCHAR NOT NULL DISTKEY,
+   song_id VARCHAR NOT NULL,
+   artist_id VARCHAR NOT NULL,
+   level VARCHAR NOT NULL,
+   session_id INT NOT NULL,
+   location VARCHAR,
+   user_agent VARCHAR
+);
 """)
 
+
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS user_table(
+   user_id VARCHAR NOT NULL SORTKEY,
+   first_name VARCHAR NOT NULL,
+   last_name VARCHAR NOT NULL,
+   gender VARCHAR NOT NULL,
+   level VARCHAR NOT NULL
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS song_table(
+   song_id VARCHAR NOT NULL SORTKEY,
+   artist_id VARCHAR NOT NULL,
+   title VARCHAR NOT NULL,
+   year SMALLINT NOT NULL,
+   duration NUMERIC(7,3) NOT NULL
+);
 """)
 
+# The position is stated to be split into its elements (lat, long) but I will use POINT instead
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artist_table(
+   artist_id VARCHAR NOT NULL SORTKEY,
+   artist_name VARCHAR NOT NULL,
+   location VARCHAR,
+   latitude NUMERIC(9,5),
+   longitude NUMERIC(9,5)
+);
 """)
 
+# It is a bit strange to vaste space in the database to store hour, date, week, ... since it is implicit part of the TIMESTAMP and easily deducted from it. 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time_table(
+   start_time TIMESTAMP NOT NULL SORTKEY,
+   hour SMALLINT,
+   day SMALLINT,
+   week SMALLINT,
+   month SMALLINT,
+   year SMALLINT,
+   weekday VARCHAR
+);
 """)
 
 # STAGING TABLES
 
 staging_events_copy = ("""
-""").format()
+COPY staging_events FROM {} credentials 'aws_iam_role={}' format as json {}
+  STATUPDATE ON region 'us-west-2';
+""").format(config['S3']['LOG_DATA'], config['IAM_ROLE']['ARN'], config['S3']['LOG_JSONPATH'])
 
 staging_songs_copy = ("""
-""").format()
+  COPY staging_songs FROM {} credentials 'aws_iam_role={}' format as json 'auto'
+  ACCEPTINVCHARS STATUPDATE ON region 'us-west-2';
+""").format(config['S3']['SONG_DATA'], config['IAM_ROLE']['ARN'])
 
 # FINAL TABLES
 
 songplay_table_insert = ("""
+INSERT INTO songplay_table (start_time, user_id, song_id, artist_id, level,
+                            session_id, location, user_agent)
+SELECT DISTINCT TIMESTAMP 'epoch' + events.ts/1000 * INTERVAL '1 second' AS start_time,
+       events.userId AS user_id, events.level AS level, events.song_id AS song_id,
+       songs.artist_id AS artist_id, events.sessionId AS session_id,
+       events.location AS location, events.userAgent AS user_agent
+FROM staging_events AS events
+JOIN staging_songs AS songs ON (events.artist = songs.artist_name) WHERE events.page = 'NextSong';
 """)
 
 user_table_insert = ("""
+INSERT INTO user_table (user_id, first_name, last_name, gender, level)
+SELECT  DISTINCT events.userId AS user_id, events.firstName AS first_name,
+        events.lastName AS last_name, events.gender AS gender,
+        events.level AS level
+FROM staging_events AS events
+WHERE events.page = 'NextSong';
 """)
 
 song_table_insert = ("""
+INSERT INTO song_table (song_id, artist_id, title, year, duration)
+SELECT  DISTINCT songs.songId AS song_id, songs.artist_id AS artist_id,
+        songs.title AS title, songs.year AS year, songs.duration AS duration
+FROM staging_songs AS songs;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artist_table (artist_id, artist_name, location, latitude, longitude)
+SELECT  DISTINCT songs.artist_id AS artist_id, songs.artist_name AS artist_name,
+        songs.location AS location, songs.latitude AS latitude, songs.longitude AS longitude
+FROM staging_songs AS songs;
 """)
 
 time_table_insert = ("""
+INSERT INTO time_table (start_time, hour, day, week, month, year, weekday)
+SELECT  DISTINCT TIMESTAMP 'epoch' + events.ts/1000 * INTERVAL '1 second' AS start_time,
+        EXTRACT(hour FROM start_time) AS hour,
+        EXTRACT(day FROM start_time) AS day,
+        EXTRACT(week FROM start_time) AS week,
+        EXTRACT(month FROM start_time) AS month,
+        EXTRACT(year FROM start_time) AS year,
+        EXTRACT(week FROM start_time) AS weekday
+FROM staging_events AS events
+WHERE events.page = 'NextSong';
 """)
 
 # QUERY LISTS
